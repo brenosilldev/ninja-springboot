02dd10e9e734bb24e552a4667df2bfd4eb3ecf8e
brenosilldev <brenosill@hotmail.com>
2025-08-20 22:46:16 -0300

    Subida de arquivos

---
+++ src/main/java/dev/brenosill/ninjaspringboot/NinjaSpringbootApplication.java
@@ -0,0 +1,13 @@
+package dev.brenosill.ninjaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NinjaSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NinjaSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/brenosill/ninjaspringboot/NinjaSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.brenosill.ninjaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NinjaSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
