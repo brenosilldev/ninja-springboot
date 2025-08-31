8c62be5f7ce4f9bcc134e438b1597496621c7f47
Breno Silva <109807534+brenosilldev@users.noreply.github.com>
2025-08-31 13:07:12 -0300

    feat: Criado arquivo de repository

---
+++ src/main/java/dev/brenosill/ninjaspringboot/Ninjas/NinjaRepository.java
@@ -0,0 +1,7 @@
+package dev.brenosill.ninjaspringboot.Ninjas;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NinjaRepository extends JpaRepository<NinjaModel,Long> {
+
+}
