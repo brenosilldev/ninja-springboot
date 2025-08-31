8c0e9d65275be4befea01aeffa90614f2a9243a3
Breno Silva <109807534+brenosilldev@users.noreply.github.com>
2025-08-31 13:07:27 -0300

    feat: Criado controller

--- src/main/java/dev/brenosill/ninjaspringboot/Ninjas/NinjaController.java
+++ src/main/java/dev/brenosill/ninjaspringboot/Ninjas/NinjaController.java
@@ -1,4 +1,16 @@
 package dev.brenosill.ninjaspringboot.Ninjas;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
 public class NinjaController {
+
+    @GetMapping("/boa")
+    public String boa(){
+        return "boa";
+    }
 }
