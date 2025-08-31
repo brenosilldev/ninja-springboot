debd7c9663d71f1c5b1b4f091972f2e02e50b893
Breno Silva <109807534+brenosilldev@users.noreply.github.com>
2025-08-31 13:06:27 -0300

    fix: Adicionado @Column

--- src/main/java/dev/brenosill/ninjaspringboot/Ninjas/NinjaModel.java
+++ src/main/java/dev/brenosill/ninjaspringboot/Ninjas/NinjaModel.java
@@ -3,6 +3,7 @@ package dev.brenosill.ninjaspringboot.Ninjas;
 
 import dev.brenosill.ninjaspringboot.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,53 +11,28 @@ import java.util.List;
 // Transforme em entidade do banco de dados
 @Entity
 @Table(name = "ninjas")
+@AllArgsConstructor
+@NoArgsConstructor // Criar os construtores
+@Data // Getter e Setters
 public class NinjaModel {
 
     @Id
+
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idninja;
+
+    @Column(length = 100,name = "nome")
     private String nome;
+    @Column(length = 100,name = "idade")
     private int idade;
+    @Column(length = 100,name = "aldeia")
     private String aldeia;
 
     //Uma unica missao por vez
     @ManyToMany
-    @JoinColumn(name = "missoes_idmissao")
-    private MissoesModel missoes;
+    @JoinTable(name = "missoes_idmissao")
+    private List<MissoesModel> missoes;
 
 
-    public NinjaModel(Long id, String nome, int idade, String aldeia, MissoesModel missoes) {
-        this.idninja = id;
-        this.nome = nome;
-        this.idade = idade;
-        this.aldeia = aldeia;
-        this.missoes = missoes;
-    }
 
-    public
-
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    public String getAldeia() {
-        return aldeia;
-    }
-
-    public void setAldeia(String aldeia) {
-        this.aldeia = aldeia;
-    }
 }
